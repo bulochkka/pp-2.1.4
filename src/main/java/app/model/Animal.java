a09ed982b5bd11348152d6044e763e1f99a48aa8
bulochkka <ram23zan@mail.ru>
2024-07-30 23:44:38 +0300

    Izmena

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,13 @@
 package app.model;
 
+import app.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
 public abstract class Animal {
 }
+
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,10 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog extends Animal{
+    public String toString(){
+        return "O daa";
+    }
+}
